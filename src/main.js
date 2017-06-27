1ecdda6eeefc9537f014902b4029d5e824f6e27f
User <nike.ua.only@gmail.com>
2017-06-27 22:06:12 +0300

    build fixes

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import LoginPage from "./login/LoginPage";
+
+ReactDOM.render(
+    <LoginPage/>,
+    document.querySelector('.app')
+);
