291b3c390a5c7b24f125dd06dba0d446bcb86627
User <nike.ua.only@gmail.com>
2017-06-27 22:05:11 +0300

    init

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import LoginPage from "./login/LoginPage";
-
-ReactDOM.render(
-    <LoginPage/>,
-    document.querySelector('.app')
-);
